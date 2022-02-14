cfa4389a2e96cfa363bfae5d24d4873c05fa0431
Sandi Ibraimoff <sam.ibraimoff@gmail.com>
2022-02-14 02:27:04 -0500

    api added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 
 import { CssBaseline, Grid } from '@material-ui/core';
 
@@ -6,7 +6,20 @@ import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
+import getPlacesData from './api';
+
 const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
+
+  useEffect(() => {
+    getPlacesData().then((data) => {
+      console.log(data);
+      setPlaces(data);
+    });
+  }, []);
+
   return (
     <Fragment>
       <CssBaseline />
@@ -17,7 +30,11 @@ const App = () => {
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map
+            coordinates={coordinates}
+            setBounds={setBounds}
+            setCoordinates={setCoordinates}
+          />
         </Grid>
       </Grid>
     </Fragment>
--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -7,22 +7,24 @@ import Rating from '@material-ui/lab';
 
 import useStyles from './styles';
 
-const Map = () => {
+const Map = ({ setBounds, setCoordinates, coordinates }) => {
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width: 600px)');
-
-  const coordinates = { lat: 0, lng: 0 };
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyCWschDxH8u8rjgZickxivpMtqA3GqoN38' }}
+        bootstrapURLKeys={{ key: process.env.key }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
         options={''}
-        onChange={''}
+        onChange={(e) => {
+          console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
         onChildClick={''}
       ></GoogleMapReact>
     </div>
